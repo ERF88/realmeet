41a537bd684d01254f29df5a8b8c441ea0cd6478
ERF88 <erf_3d@hotmail.com>
2025-01-06 18:14:23 -0300

    Adicionando 2 módulos no projeto Maven service e client

---
+++ client/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.github.erf88;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
