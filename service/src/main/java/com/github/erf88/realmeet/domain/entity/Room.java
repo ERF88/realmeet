26df7b8025485ff66cbea8fc07f8f5cd820cc829
erf88 <erf_3d@hotmail.com>
2025-01-15 20:45:23 -0300

    criando a classe Room

---
+++ service/src/main/java/com/github/erf88/realmeet/domain/entity/Room.java
@@ -0,0 +1,76 @@
+package com.github.erf88.realmeet.domain.entity;
+
+import java.util.Objects;
+import org.hibernate.proxy.HibernateProxy;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@Entity
+public class Room {
+
+    @Id
+    private Long id;
+    private String name;
+    private Integer seats;
+    private Boolean active;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Room room = (Room) o;
+        return getId() != null && Objects.equals(getId(), room.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Long id;
+        private String name;
+        private Integer seats;
+        private Boolean active;
+
+        private Builder() {
+        }
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder seats(Integer seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public Builder active(Boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public Room build() {
+            return new Room(id, name, seats, active);
+        }
+
+    }
+}
