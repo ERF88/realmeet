777d9d09d97991e8cb0a3843c22eca4b86c8f093
erf88 <erf_3d@hotmail.com>
2025-01-17 07:11:00 -0300

    Teste unitário RoomService

---
+++ service/src/test/java/com/github/erf88/realmeet/core/BaseUnitTest.java
@@ -0,0 +1,8 @@
+package com.github.erf88.realmeet.core;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class BaseUnitTest {
+}
--- service/src/test/java/com/github/erf88/realmeet/unit/RoomServiceUnitTest.java
+++ service/src/test/java/com/github/erf88/realmeet/unit/RoomServiceUnitTest.java
@@ -1,33 +1,44 @@
 package com.github.erf88.realmeet.unit;
 
 import static com.github.erf88.realmeet.utils.MapperUtils.roomMapper;
-import static com.github.erf88.realmeet.utils.TestConstants.*;
+import static com.github.erf88.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
 import static com.github.erf88.realmeet.utils.TestDataCreator.newRoomBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 import com.github.erf88.realmeet.api.model.RoomDTO;
 import com.github.erf88.realmeet.core.BaseUnitTest;
 import com.github.erf88.realmeet.domain.entity.Room;
+import com.github.erf88.realmeet.domain.repository.RoomRepository;
 import com.github.erf88.realmeet.mapper.RoomMapper;
+import com.github.erf88.realmeet.service.RoomService;
+import com.github.erf88.realmeet.utils.MapperUtils;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+class RoomServiceUnitTest extends BaseUnitTest {
 
-class RoomMapperUnitTest extends BaseUnitTest {
+    private RoomService victim;
 
-    private RoomMapper victim;
+    @Mock
+    private RoomRepository roomRepository;
 
     @BeforeEach
     void setEach() {
-        victim = roomMapper();
+        victim = new RoomService(roomRepository, roomMapper());
     }
 
     @Test
-    void testToRoomDTO() {
+    void testGetRoomById() {
         Room room = newRoomBuilder().id(DEFAULT_ROOM_ID).build();
-        RoomDTO roomDTO = victim.toRoomDTO(room);
+        when(roomRepository.findById(DEFAULT_ROOM_ID)).thenReturn(Optional.of(room));
+
+        RoomDTO roomDTO = victim.getRoomById(DEFAULT_ROOM_ID);
 
         assertEquals(room.getId(), roomDTO.getId());
         assertEquals(room.getName(), roomDTO.getName());
