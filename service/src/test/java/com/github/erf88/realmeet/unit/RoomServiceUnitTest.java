e9ee2e065261064a0141278e3777daf647c5fd63
erf88 <erf_3d@hotmail.com>
2025-01-17 23:43:10 -0300

    Outros cenários de testes unitários

--- service/src/test/java/com/github/erf88/realmeet/unit/RoomServiceUnitTest.java
+++ service/src/test/java/com/github/erf88/realmeet/unit/RoomServiceUnitTest.java
@@ -4,6 +4,7 @@ import static com.github.erf88.realmeet.utils.MapperUtils.roomMapper;
 import static com.github.erf88.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
 import static com.github.erf88.realmeet.utils.TestDataCreator.newRoomBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -11,6 +12,7 @@ import com.github.erf88.realmeet.api.model.RoomDTO;
 import com.github.erf88.realmeet.core.BaseUnitTest;
 import com.github.erf88.realmeet.domain.entity.Room;
 import com.github.erf88.realmeet.domain.repository.RoomRepository;
+import com.github.erf88.realmeet.exception.ResourceNotFoundException;
 import com.github.erf88.realmeet.mapper.RoomMapper;
 import com.github.erf88.realmeet.service.RoomService;
 import com.github.erf88.realmeet.utils.MapperUtils;
@@ -34,15 +36,23 @@ class RoomServiceUnitTest extends BaseUnitTest {
     }
 
     @Test
-    void testGetRoomById() {
+    void testGetRoomByIdSuccess() {
         Room room = newRoomBuilder().id(DEFAULT_ROOM_ID).build();
-        when(roomRepository.findById(DEFAULT_ROOM_ID)).thenReturn(Optional.of(room));
+        when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, Boolean.TRUE)).thenReturn(Optional.of(room));
 
         RoomDTO roomDTO = victim.getRoomById(DEFAULT_ROOM_ID);
 
         assertEquals(room.getId(), roomDTO.getId());
         assertEquals(room.getName(), roomDTO.getName());
         assertEquals(room.getSeats(), roomDTO.getSeats());
+    }
+
+    @Test
+    void testGetRoomByIdNotFound() {
+        Room room = newRoomBuilder().id(DEFAULT_ROOM_ID).build();
+        when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, Boolean.TRUE)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> victim.getRoomById(DEFAULT_ROOM_ID));
     }
 
 }
