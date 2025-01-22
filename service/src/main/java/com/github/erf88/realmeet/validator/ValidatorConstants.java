357bdd629ead1de0cd719b03c088dba399740d3f
erf88 <erf_3d@hotmail.com>
2025-01-22 20:37:38 -0300

    Criando métodos comuns de validação no ValidatorUtils

---
+++ service/src/main/java/com/github/erf88/realmeet/validator/ValidatorConstants.java
@@ -0,0 +1,10 @@
+package com.github.erf88.realmeet.validator;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public final class ValidatorConstants {
+
+    public static final String MISSING = ".missing";
+    public static final String EXCEEDS_MAX_LENGTH = ".exceedsMaxLength";
+}
--- service/src/main/java/com/github/erf88/realmeet/validator/ValidatorUtils.java
+++ service/src/main/java/com/github/erf88/realmeet/validator/ValidatorUtils.java
@@ -1,6 +1,11 @@
 package com.github.erf88.realmeet.validator;
 
+import static com.github.erf88.realmeet.validator.ValidatorConstants.EXCEEDS_MAX_LENGTH;
+import static com.github.erf88.realmeet.validator.ValidatorConstants.MISSING;
+import static java.util.Objects.isNull;
+
 import com.github.erf88.realmeet.exception.InvalidRequestException;
+import java.util.Objects;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
@@ -10,5 +15,29 @@ public final class ValidatorUtils {
         if (validationErrors.hasErrors()) {
             throw new InvalidRequestException(validationErrors);
         }
+    }
+
+    public static boolean validateRequired(String field, String fieldName, ValidationErrors validationErrors) {
+        if(field.isBlank()) {
+            validationErrors.add(fieldName, fieldName.concat(MISSING));
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateRequired(Object field, String fieldName, ValidationErrors validationErrors) {
+        if(isNull(field)) {
+            validationErrors.add(fieldName, fieldName.concat(MISSING));
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateMaxLength(String field, String fieldName, int maxLength, ValidationErrors validationErrors) {
+        if(!field.isBlank() && field.trim().length() > maxLength) {
+            validationErrors.add(fieldName, fieldName.concat(EXCEEDS_MAX_LENGTH));
+            return false;
+        }
+        return true;
     }
 }
