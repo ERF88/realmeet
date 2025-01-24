81dd988dab9107d4e74a4604df08cfafe3eb588c
erf88 <erf_3d@hotmail.com>
2025-01-23 23:21:15 -0300

    Criando o endpoint de atualização de sala

--- service/src/main/java/com/github/erf88/realmeet/controller/RoomController.java
+++ service/src/main/java/com/github/erf88/realmeet/controller/RoomController.java
@@ -6,13 +6,14 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 import com.github.erf88.realmeet.api.facade.RoomsApi;
 import com.github.erf88.realmeet.api.model.CreateRoomDTO;
 import com.github.erf88.realmeet.api.model.RoomDTO;
+import com.github.erf88.realmeet.api.model.UpdateRoomDTO;
 import com.github.erf88.realmeet.service.RoomService;
 import com.github.erf88.realmeet.util.ResponseEntityUtils;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,6 +34,13 @@ public class RoomController implements RoomsApi {
 
     @Override
     public CompletableFuture<ResponseEntity<Void>> deleteRoom(Long id) {
-        return runAsync(() -> roomService.deleteRoom(id), controllersExecutor).thenApply(ResponseEntityUtils::noContent);
+        return runAsync(() -> roomService.deleteRoom(id), controllersExecutor)
+            .thenApply(ResponseEntityUtils::noContent);
+    }
+
+    @Override
+    public CompletableFuture<ResponseEntity<Void>> updateRoom(Long id, UpdateRoomDTO updateRoomDTO) {
+        return runAsync(() -> roomService.updateRoom(id, updateRoomDTO), controllersExecutor)
+                .thenApply(ResponseEntityUtils::noContent);
     }
 }
