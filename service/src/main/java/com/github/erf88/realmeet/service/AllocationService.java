f86cd7c72993288a6acb7cc90c2f95b2510f7e24
erf88 <erf_3d@hotmail.com>
2025-01-30 19:42:35 -0300

    Implementando a lógica de criação de uma nova allocation

--- service/src/main/java/com/github/erf88/realmeet/service/AllocationService.java
+++ service/src/main/java/com/github/erf88/realmeet/service/AllocationService.java
@@ -6,6 +6,7 @@ import com.github.erf88.realmeet.domain.entity.Allocation;
 import com.github.erf88.realmeet.domain.entity.Room;
 import com.github.erf88.realmeet.domain.repository.AllocationRepository;
 import com.github.erf88.realmeet.domain.repository.RoomRepository;
+import com.github.erf88.realmeet.exception.ResourceNotFoundException;
 import com.github.erf88.realmeet.mapper.AllocationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,9 @@ public class AllocationService {
     private final RoomRepository roomRepository;
 
     public AllocationDTO createAllocation(CreateAllocationDTO createAllocationDTO) {
-        Allocation allocation = allocationMapper.toAllocation(createAllocationDTO, Room.newBuilder().build());
+        Room room = roomRepository.findById(createAllocationDTO.getRoomId())
+            .orElseThrow(() -> new ResourceNotFoundException("Room", createAllocationDTO.getRoomId()));
+        Allocation allocation = allocationMapper.toAllocation(createAllocationDTO, room);
         allocationRepository.save(allocation);
         return allocationMapper.toAllocationDTO(allocation);
     }
