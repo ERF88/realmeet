bbca756fcc9e2c1b05feb20143523401d70d262d
erf88 <erf_3d@hotmail.com>
2025-02-06 16:45:16 -0300

    Definindo o método de update em AllocationRepository

--- service/src/main/java/com/github/erf88/realmeet/domain/repository/AllocationRepository.java
+++ service/src/main/java/com/github/erf88/realmeet/domain/repository/AllocationRepository.java
@@ -2,6 +2,7 @@ package com.github.erf88.realmeet.domain.repository;
 
 import com.github.erf88.realmeet.domain.entity.Allocation;
 import com.github.erf88.realmeet.domain.entity.Room;
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,4 +11,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AllocationRepository extends JpaRepository<Allocation, Long> {}
+public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Allocation a SET a.subject = :subject, a.startAt = :startAt, a.endAt = :endAt WHERE r.id = :id")
+    void updateAllocation(
+        @Param("id") Long id,
+        @Param("subject") String subject,
+        @Param("startAt") OffsetDateTime startAt,
+        @Param("endAt") OffsetDateTime endAt
+    );
+}
