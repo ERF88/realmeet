71f0c85cd0ae9871f3d4cfb1905f4aeefc9b6a77
erf88 <erf_3d@hotmail.com>
2025-02-19 23:19:00 -0300

    Escrevendo os testes unitários da classe PageUtils parte 2 de 2

--- service/src/test/java/com/github/erf88/realmeet/unit/PageUtilsUnitTest.java
+++ service/src/test/java/com/github/erf88/realmeet/unit/PageUtilsUnitTest.java
@@ -1,11 +1,13 @@
 package com.github.erf88.realmeet.unit;
 
+import static com.github.erf88.realmeet.domain.entity.Allocation.SORTABLE_FIELDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.github.erf88.realmeet.core.BaseUnitTest;
-import com.github.erf88.realmeet.domain.entity.Allocation;
+import com.github.erf88.realmeet.exception.InvalidOrderByFieldException;
 import com.github.erf88.realmeet.util.PageUtils;
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -15,7 +17,7 @@ public class PageUtilsUnitTest extends BaseUnitTest {
     @Test
     void testNewPageableWhenPageIsNullAndLimitIsNullAndOrderByIsNull() {
         Pageable pageable = PageUtils
-            .newPageable(null, null, 10, null, Allocation.SORTABLE_FIELDS);
+            .newPageable(null, null, 10, null, SORTABLE_FIELDS);
 
         assertEquals(0, pageable.getPageNumber());
         assertEquals(10, pageable.getPageSize());
@@ -26,7 +28,63 @@ public class PageUtilsUnitTest extends BaseUnitTest {
     void testNewPageableWhenPageIsNegative() {
         assertThrows(
             IllegalArgumentException.class,
-            () -> PageUtils.newPageable(-1, null, 10, null, Allocation.SORTABLE_FIELDS)
+            () -> PageUtils.newPageable(-1, null, 10, null, SORTABLE_FIELDS)
+        );
+    }
+
+    @Test
+    void testNewPageableWhenLimitIsInvalid() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> PageUtils.newPageable(null, 0, 10, null, SORTABLE_FIELDS)
+        );
+    }
+
+    @Test
+    void testNewPageableWhenLimitExceedsMaximum() {
+        Pageable pageable = PageUtils
+            .newPageable(null, 30, 10, null, SORTABLE_FIELDS);
+
+        assertEquals(10, pageable.getPageSize());
+    }
+
+    @Test
+    void testNewPageableWhenOrderByIsNull() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> PageUtils.newPageable(null, 0, 10, null, null)
+        );
+    }
+
+    @Test
+    void testNewPageableWhenOrderByIsEmpty() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> PageUtils.newPageable(null, 0, 10, null, Collections.emptyList())
+        );
+    }
+
+    @Test
+    void testNewPageableWhenOrderByAscIsValid() {
+        Pageable pageable = PageUtils
+            .newPageable(null, null, 10, SORTABLE_FIELDS.getFirst(), SORTABLE_FIELDS);
+
+        assertEquals(Sort.by(Sort.Order.asc(SORTABLE_FIELDS.getFirst())), pageable.getSort());
+    }
+
+    @Test
+    void testNewPageableWhenOrderByDescIsValid() {
+        Pageable pageable = PageUtils
+            .newPageable(null, null, 10, "-".concat(SORTABLE_FIELDS.getFirst()), SORTABLE_FIELDS);
+
+        assertEquals(Sort.by(Sort.Order.desc(SORTABLE_FIELDS.getFirst())), pageable.getSort());
+    }
+
+    @Test
+    void testNewPageableWhenOrderByFieldIsInvalid() {
+        assertThrows(
+            InvalidOrderByFieldException.class,
+            () -> PageUtils.newPageable(null, null, 10, "invalid", SORTABLE_FIELDS)
         );
     }
 }
